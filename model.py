8cae7b8d10e929fc1489114bb76982ad7b8fc52b
Aymeric Palaric <aymeric.palaric@student-cs.fr>
2024-03-23 18:25:11 +0100

    added a datacollector, a  method and updated the  method for the Environment

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
 import random
 
 from agents import GreenRobot
-from objects import GreenWasteAgent, HazardGrid
+from objects import GreenWasteAgent, HazardGrid, WasteAgent
 
 
 class Environnement(Model):
@@ -14,7 +15,11 @@ class Environnement(Model):
         self.num_waste = Nw
 
         self.grid_len = L
-        self.scheduler = RandomActivation(self)  
+        self.scheduler = RandomActivation(self)
+        self.datacollector = DataCollector(
+            agent_reporters={"Carry": "carry"},
+            model_reporters={"NbWaste": lambda m: len([a for a in m.schedule.agents if isinstance(a, WasteAgent)])}
+        )
         
         # Grid
         self.grid = HazardGrid (L, 0, 1)
@@ -32,6 +37,21 @@ class Environnement(Model):
             a = GreenRobot(i, self)  # Pass 'self' to give the agent access to the model
             #self.scheduler.add(a)
 
+    def do(self, agent, action, **kwargs):
+        if action == "move":
+            self.grid.move_agent(agent, kwargs["pos"])
+        elif action == "pick_up":
+            # remove the waste picked up from the grid
+            self.grid.remove(kwargs["waste"])
+            agent.carry += 1
+        elif action == "drop":
+            # add the waste dropped to the grid
+            self.grid.place(kwargs["waste"], agent.pos)
+            agent.carry == 0
+        else:
+            print("Unknown action: ", action)
+
+
     # 1. this method performs only 1 step for all agents
     def one_step(self):
         # get waste position
@@ -39,6 +59,8 @@ class Environnement(Model):
 
         
         distance_robot = []
+
+        self.datacollector.collect(self)
         self.scheduler.step()
 
 
