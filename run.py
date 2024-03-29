76ed658655afb3950b085475a70aac316fa695a9
Aymeric Palaric <aymeric.palaric@student-cs.fr>
2024-03-29 13:04:05 +0100

    main run program

---
+++ run.py
@@ -0,0 +1,20 @@
+from model import Environnement
+import seaborn as sns
+
+def main(nsteps=1):
+    environnement = Environnement(2, 5, 15)
+    environnement.run_n_steps(nsteps)
+    environnement.grid.draw()
+    agent_inventory = environnement.datacollector.get_agent_vars_dataframe()
+    last_step = agent_inventory.index.get_level_values('Step').max()
+    agent_inventory = agent_inventory.xs(last_step, level="Step")["Carry"]
+    g = sns.histplot(agent_inventory, discrete=True)
+    g.set(
+        xlabel="Number of waste carried",
+        ylabel="Number of robots",
+        title="Number of waste carried by each robot"
+    )
+
+
+if __name__ == "__main__":
+    main(10)
