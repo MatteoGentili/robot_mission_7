48ac6dbfa232ae870cf69810d0e6829b8c3ab499
aymeric.palaric@aaico.com <aymeric.palaric@tudent-cs.fr>
2024-04-05 16:37:06 +0200

    init communicating agents

--- agents.py
+++ agents.py
@@ -111,7 +111,8 @@ class GreenRobot(Robot):
 
 
 class CommunicatingGreenRobot(GreenRobot):
-    pass
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model, pos)
 
 
 class YellowRobot(Robot):
@@ -141,7 +142,8 @@ class YellowRobot(Robot):
         self.percepts = self.knowledge
 
 class CommunicatingYellowRobot(YellowRobot):
-    pass
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model, pos)
 
 class RedRobot(Robot):
     """
@@ -215,4 +217,5 @@ class RedRobot(Robot):
                 return {"action": action, "waste": self.inventory[0]}
 
 class CommunicatingRedRobot(RedRobot):
-    pass
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model, pos)
